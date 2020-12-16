422fbf009b058d05833b263140c4f4313c882c28
Christopher Donahue <donahue.chris@gmail.com>
2020-12-15 17:04:31 -0800

    bug in write_csv

--- DataCleaner.py
+++ DataCleaner.py
@@ -80,9 +80,9 @@ class DataCleaner:
 
 	def write_csv(self,filename):
 		# couldn't figure out how to avoid these stupid loops:
-		for i,x in enumerate(range(1,self.conf.shape[1],3)):
+		for i,x in enumerate(range(1,self.df_orig.shape[1],3)):
 			self.df_orig.iloc[:,x] = self.x.iloc[:,i]
-		for i,x in enumerate(range(2,self.conf.shape[1],3)):
+		for i,x in enumerate(range(2,self.df_orig.shape[1],3)):
 			self.df_orig.iloc[:,x] = self.y.iloc[:,i]
 		self.df_orig.to_csv(filename, index=False)
 
